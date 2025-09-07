b0ba11846ee293cca42161dd2ac18458747469f9
aakritrajput <aakritrajput87@gmail.com>
2025-09-07 18:41:25 +0530

    Connected Database

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+
+const connectDB = async() => {
+    try {
+        const con_inst = await mongoose.connect(`${process.env.MONGODB_URI}/TaskMaaDB`)
+        console.log('Congrats... Database connected !!')
+    } catch (error) {
+        console.log("Upps.. There was some error connecting to the database: ", error.message)
+        process.exit(1)
+    }
+}
+
+export default connectDB
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
+import dotenv from 'dotenv'
 import app from './app.js'
+import connectDB from './db/index.js'
 
-app.get('/', (_, res)=>{
-    res.json({data: 'hey i am good'})
+dotenv.config({  // just a double check as already configured in start or dev script
+    path: './.env'
 })
 
-app.listen(5000, ()=>{
-    console.log('app connected successfully')
+connectDB().then(()=>{
+    app.on("error", (error)=>{
+        console.log("Error: ", error)
+        throw error
+    })
+    app.listen(process.env.PORT || 5000, ()=>{
+        console.log(`hurray... Server started running at port: ${process.env.PORT}`)
+    })
+}).catch((error)=> {
+    console.log("No way... Connection to database failed !!", error)
 })

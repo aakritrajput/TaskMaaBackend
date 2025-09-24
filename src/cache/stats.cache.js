16a14f48e582e4591150c734f804fb5362542cca
aakritrajput <aakritrajput87@gmail.com>
2025-09-24 15:20:57 +0530

    Created function for invalidating cache data for performance of the user

--- src/cache/stats.cache.js
+++ src/cache/stats.cache.js
@@ -2,7 +2,7 @@ import { redis } from "../db/redis";
 
 const performanceFromCache = async(userId) => {
     try {
-        key = `user:${userId}:performance`
+        const key = `user:${userId}:performance`
         const data = await redis.get(key)
         return data ? JSON.parse(data) : null;
     } catch (error) {
@@ -13,8 +13,19 @@ const performanceFromCache = async(userId) => {
 
 const performanceToCache = async(userId, data, ttl=300) => { // cache for 5 minutes - as this is something which will update frequently so that's why for 5 minutes but if updated within that then we can invalidate manually !!
     try {
-        key = `user:${userId}:performance`
+        const key = `user:${userId}:performance`
         const response = await redis.set(key, JSON.stringify(data), 'EX', ttl)
+        return response ;
+    } catch (error) {
+        console.error("Redis Cache Save Error:", error.message);
+        return null;
+    }
+}
+
+const invalidatePerformanceCache = async(userId) => {
+    try {
+        const key = `user:${userId}:performance`
+        const response = await redis.del(key)
         return response ;
     } catch (error) {
         console.error("Redis Cache Save Error:", error.message);
@@ -25,4 +36,5 @@ const performanceToCache = async(userId, data, ttl=300) => { // cache for 5 minu
 export {
     performanceFromCache,
     performanceToCache,
+    invalidatePerformanceCache,
 }

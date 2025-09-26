304820325954641cf332c3ad4cb18266f0a7711f
aakritrajput <aakritrajput87@gmail.com>
2025-09-26 20:32:54 +0530

    Integrated old http app with websocket app for chatting purpose

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import cookieParser from 'cookie-parser'
 import express from 'express'
 import cors from 'cors'
+import http from 'http';
+import chatSocket from './utils/sockets/ChatSocket.js';
 
 const app = express()
 
@@ -14,4 +16,8 @@ app.use(express.json({limit: '50kb'}))
 app.use(express.urlencoded({extended: true, limit: '25kb'}))
 app.use(express.static('public'))
 
-export default app
+const server = http.createServer(app);
+
+chatSocket(server);
+
+export default server

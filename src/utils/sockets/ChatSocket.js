2884e196e70a8e869bfcf24bf2fe4b3f440adbff
aakritrajput <aakritrajput87@gmail.com>
2025-09-28 16:46:46 +0530

    Created Chat sockets for one to one chat with proper efficient and real time delievery and storage

--- src/utils/sockets/ChatSocket.js
+++ src/utils/sockets/ChatSocket.js
@@ -1,4 +1,5 @@
 import { Server } from "socket.io";
+import { getSocketIdOfUser, invalidateSocketIdForUser, socketIdForUser, messageToCacheQueue, storeOfflineMessage } from "../../cache/socket.cache";
 
 export default function chatSocket(server){
     const io = new Server(server, {
@@ -8,11 +9,46 @@ export default function chatSocket(server){
         }
     })
 
-    io.on('connection', (socket) => {
-        console.log('User connected: ', socket.id);
+    const onlineUsers = new Map(); // map for storing online users in-memory -- will not take much space even if lakhs of users then also 10 to 15 mb of RAM consumed and can use this to quickly lookup for users socketID's
 
-        socket.on('join_chat', (userId) => socket.join(userId));
-        socket.on('send_message', (data) => io.to(data.receiverId).emit('recieve_message', data));
-        socket.on('disconnect', () => console.log('User disconnected: ', socket.io))
+    io.on('connection', async(socket) => { // right now we are just handling the basic errors and it can be extended to handle all deeper errors like which may come from caching functions 
+        try {
+            console.log('User connected: ', socket.id);
+            const userId = socket.handshake.query.userId
+            if (!userId) return ;
+
+            onlineUsers.set(userId, socket.id) // for quickly looking up socket id's in memory 
+            await socketIdForUser(userId, socket.id); // if users not in the memory of the same server then we can query redis for the users socketIDs
+
+            // ----------- One to One chats ---------------
+
+            socket.on('send-message', async({senderId, recieverId, message})=> {
+                let receiverSocketId = onlineUsers.get(recieverId)
+                if(!receiverSocketId){
+                    receiverSocketId = await getSocketIdOfUser(recieverId);
+                }
+
+                const timestamp = Date.now();
+
+                if (receiverSocketId) {
+                    socket.to(receiverSocketId).emit('recieve-message', { senderId, recieverId, message, timestamp });
+                    await messageToCacheQueue({senderId, recieverId, message, timestamp})
+                } else {
+                    console.log(`User ${recieverId} is offline. Storing message for later delivery.`);
+                    await storeOfflineMessage(recieverId, { senderId, message, timestamp });
+                }
+            })
+
+            // -------- On disconnect -----------
+
+            socket.on('disconnect', async() => {
+                console.log('User disconnected: ', socket.id)
+                onlineUsers.delete(userId)
+                await invalidateSocketIdForUser(userId);
+            });
+
+        } catch (error) {
+            console.log('Error during connection: ', error)
+        }
     })
 }

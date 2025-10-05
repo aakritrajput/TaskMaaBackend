3132dc22b345568a3e873d587be1be34b0159b40
aakritrajput <aakritrajput87@gmail.com>
2025-10-05 21:36:35 +0530

    Tested Inference client from hugging face

---
+++ src/services/aiService.js
@@ -0,0 +1,21 @@
+import { InferenceClient } from "@huggingface/inference";
+
+const HF_TOKEN = process.env.HUGGINGFACE_API_KEY ;
+
+const client = new InferenceClient(HF_TOKEN);
+
+const out = await client.chatCompletion({
+    model: "meta-llama/Llama-3.1-8B-Instruct",
+    messages: [{ role: "user", content: "Hello, nice to meet you!" }],
+    max_tokens: 512
+});
+
+console.log("This is the response: ", out.choices);
+
+// for await (const chunk of client.chatCompletionStream({
+//   model: "meta-llama/Llama-3.1-8B-Instruct",
+//   messages: [{ role: "user", content: "Hello, nice to meet you!" }],
+//   max_tokens: 512
+// })) {
+//   console.log(chunk.choices[0].delta.content);
+// }

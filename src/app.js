b25902e44b1eb90a6a079a84ec45c1f66ecbd8b2
aakritrajput <aakritrajput87@gmail.com>
2025-10-14 19:20:24 +0530

    Updated the scripts in package.json for normal workflow instead of AI one and added the imports and use of user routers in main app

--- src/app.js
+++ src/app.js
@@ -2,7 +2,10 @@ import cookieParser from 'cookie-parser'
 import express from 'express'
 import cors from 'cors'
 import http from 'http';
-import chatSocket from './utils/sockets/ChatSocket.js';
+import chatSocket from './sockets/ChatSocket.js';
+
+// ---- Importing routes -------
+import userRouter from './routes/user.routes.js'
 
 const app = express()
 
@@ -15,6 +18,9 @@ app.use(cookieParser())
 app.use(express.json({limit: '50kb'}))
 app.use(express.urlencoded({extended: true, limit: '25kb'}))
 app.use(express.static('public'))
+
+
+app.use('/api/user', userRouter)
 
 const server = http.createServer(app);
 

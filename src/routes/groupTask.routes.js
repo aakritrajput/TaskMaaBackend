455ff39d69e4ced0992700894c2cafa355c2f520
aakritrajput <aakritrajput87@gmail.com>
2025-10-22 14:14:37 +0530

    Created routes for fetching members of the group task

--- src/routes/groupTask.routes.js
+++ src/routes/groupTask.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
-import { createGroupTask, getMyGroupTasks, getPublicGroupTasks } from "../controllers/groupTask.controller.js";
+import { createGroupTask, editGroupTask, getMembersOfGroupTask, getMyGroupTasks, getPublicGroupTasks } from "../controllers/groupTask.controller.js";
 
 const router = Router()
 
 router.route('/myGroupTasks').get(verifyJwt, getMyGroupTasks)
 router.route('/publicGroupTasks').get(verifyJwt, getPublicGroupTasks)
 router.route('/createGroupTask').post(verifyJwt, createGroupTask)
+router.route('/groupTaskMembers/:id').get(verifyJwt, getMembersOfGroupTask)
+router.route('/editGroupTask/:groupTaskId').post(verifyJwt, editGroupTask)
 
 export default router;

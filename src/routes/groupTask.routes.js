fae1484424623913bab35d6d35781a490ed16f13
aakritrajput <aakritrajput87@gmail.com>
2025-10-23 15:00:45 +0530

    Routes for final toggling group task status at admin level and individual level

--- src/routes/groupTask.routes.js
+++ src/routes/groupTask.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
-import { createGroupTask, deleteGroupTask, editGroupTask, getMembersOfGroupTask, getMyGroupTasks, getPublicGroupTasks } from "../controllers/groupTask.controller.js";
+import { createGroupTask, deleteGroupTask, editGroupTask, getMembersOfGroupTask, getMyGroupTasks, getPublicGroupTasks, markCompleted, participateInPublicGroupTask, toggleGroupTaskStatus } from "../controllers/groupTask.controller.js";
 
 const router = Router()
 
@@ -10,5 +10,8 @@ router.route('/createGroupTask').post(verifyJwt, createGroupTask)
 router.route('/groupTaskMembers/:id').get(verifyJwt, getMembersOfGroupTask)
 router.route('/editGroupTask/:groupTaskId').post(verifyJwt, editGroupTask)
 router.route('/deleteGroupTask/:groupTaskId').delete(verifyJwt, deleteGroupTask)
+router.route('/markComplete/:groupTaskId').get(verifyJwt, markCompleted)
+router.route('/toggleGroupTask/:groupTaskId').get(verifyJwt, toggleGroupTaskStatus)
+router.route('/participateInPublicGroupTask/:groupTaskId').get(verifyJwt, participateInPublicGroupTask)
 
 export default router;

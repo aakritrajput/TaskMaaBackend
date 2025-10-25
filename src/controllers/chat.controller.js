c43d31eeb8c49f91baec32503239779f1954f348
aakritrajput <aakritrajput87@gmail.com>
2025-10-25 17:29:54 +0530

    Added controller to get users chat interface that includes all initial chat data

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -1,4 +1,4 @@
-import { addGroupChatMembersToCache } from "../cache/socket.cache.js";
+import { addChatIdToMultipleUsers, addGroupChatMembersToCache, getChatIdsOfUser, storeChatIdsOfUser } from "../cache/socket.cache.js";
 import { checkIfMyFriend, getFriendsFromCache } from "../cache/user.cache.js";
 import { Chat } from "../models/chatModels/chat.model.js";
 import { ApiError } from "../utils/ApiError.js";
@@ -27,6 +27,8 @@ const createOneToOneChat = async(req, res) => {
             ]
         })
 
+        await storeChatIdsOfUser(userId, chat._id)
+
         res.status(200).json(new ApiResponse(200, chat, "Successfully created your chat !!"))
     } catch (error) {
         res.status(error.statusCode || 500).json({message: error.message || 'Oops.. There was some Error initiating your chat !!'})
@@ -48,7 +50,7 @@ const createGroupChat = async(req, res) => {
         const participantIds = participants.filter((participant) => friends.includes(participant))
         participants = participantIds.map((participant) => ({user: participant, role: 'participant'}))
 
-        const chat = await Chat.create({ 
+        const chat = await Chat.create({
             isGroupChat: true,
             groupName: name,
             users: [                           // in one to one chat our both users will be roled as 'admin'
@@ -58,6 +60,7 @@ const createGroupChat = async(req, res) => {
         })
 
         await addGroupChatMembersToCache(chat._id, [userId, ...participantIds])
+        await addChatIdToMultipleUsers([...participantIds, userId], chat._id)
 
         res.status(200).json(new ApiResponse(200, chat, "Successfully created your group chat !!"))
     } catch (error) {
@@ -65,7 +68,31 @@ const createGroupChat = async(req, res) => {
     }
 }
 
+const getChatInterface = async(req, res) => {
+    try {
+        const userId = req.user._id
+        if(!userId){
+            throw new ApiError(401, "Unauthorized Request !!")
+        }
+
+        const chatIds = await getChatIdsOfUser(userId) // at this stage we are only getting chatIds from cache and will not enquire db for this but in future can add extra logic for the same 
+        // AGAIN: If by chance there would not be any chatIds so we need to figure out a solution (if our app in future will become daily use to app ) for getting chatIds of the user
+        if(chatIds.length == 0){
+            return res.status(200).json(new ApiResponse(200, [], "You don't have any chatIds yet"))
+        }
+
+        const chats = await Chat.find({
+            _id: {$in: chatIds}
+        }).populate("users.user", "username profilePicture").sort({ updatedAt: -1 });
+
+        res.status(200).json(new ApiResponse(200, chats, "Here are your chats !!"))
+    } catch (error) {
+        res.status(error.statusCode || 500).json({message: error.message || 'Oops.. There was some Error creating your group chat !!'})
+    }
+}
+
 export {
     createOneToOneChat,
-    createGroupChat
+    createGroupChat,
+    getChatInterface,
 }

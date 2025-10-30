c91b191236d97f2c797953c8509ec67990fb9e9f
aakritrajput <aakritrajput87@gmail.com>
2025-10-30 19:33:38 +0530

    fixed errors in one to one chats

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -1,36 +1,45 @@
 import { addChatIdToMultipleUsers, messagesToCache, messagesFromCache, addGroupChatMembersToCache, getChatIdsOfUser, storeChatIdsOfUser } from "../cache/socket.cache.js";
-import { checkIfMyFriend, getFriendsFromCache } from "../cache/user.cache.js";
+import { getFriendsFromCache } from "../cache/user.cache.js";
 import { Chat } from "../models/chatModels/chat.model.js";
 import { Message } from "../models/chatModels/message.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { checkIsMyFriend } from "./user.controller.js";
 
 const createOneToOneChat = async(req, res) => {
     try {
-        const {friendId} = req.params 
-        if(!chatWith){
+        const {friendId} = req.params
+        if(!friendId){
             throw new ApiError(400, "Please provide the userId of the user with whom you want to chat !!")
         }
         const userId = req.user._id
         if(!userId){
             throw new ApiError(401, "Unauthorized Request !!")
         }
-        const isFriend = await checkIfMyFriend(userId, friendId)
+        const isFriend = await checkIsMyFriend(userId, friendId)
         if(!isFriend){
+            console.log('not friend !!!')
             throw new ApiError(400, "Please send a friend request to the user and be his friend to chat !!")
         }
 
-        const chat = await Chat.create({ 
-            isGroupChat: false,
-            users: [                           // in one to one chat our both users will be roled as 'admin'
-                {user: userId, role:'admin'},
-                {user: friendId, role: 'admin'},
-            ]
-        })
-
-        await storeChatIdsOfUser(userId, chat._id)
-
-        res.status(200).json(new ApiResponse(200, chat, "Successfully created your chat !!"))
+        try {
+            const chat = await Chat.create({ 
+                isGroupChat: false,
+                users: [                           // in one to one chat our both users will be roled as 'admin'
+                    {user: userId, role:'admin'},
+                    {user: friendId, role: 'admin'},
+                ]
+            })
+    
+            await addChatIdToMultipleUsers([userId, friendId], chat._id)
+    
+            res.status(200).json(new ApiResponse(200, chat, "Successfully created your chat !!"))
+        } catch (error) {
+            if (err.code === 11000) {
+              // Duplicate uniquePairKey
+              throw new ApiError(400, 'The chat room already exist with given user !!')
+            }
+        }
     } catch (error) {
         res.status(error.statusCode || 500).json({message: error.message || 'Oops.. There was some Error initiating your chat !!'})
     }
